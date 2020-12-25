edf60f1e359befc766a4f5be69103f1dcedaae53
roig7 <roig.alberto@hotmail.com>
2020-12-25 19:06:55 +0100

    Creacion del proyecto

---
+++ practica2complejidad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica2complejidad
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
